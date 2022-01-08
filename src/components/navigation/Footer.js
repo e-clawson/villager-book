bf157b6729eec5c48709a83e50c69ebab748f7b1
Elizabeth Clawson <emclawson1@gmail.com>
2022-01-08 14:44:01 -0600

    update footer link styling

--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -5,12 +5,12 @@ export const Footer = () => {
         <div>
             <div className="row" style={{display:"flex", height:"50px", marginBottom:"-50px"}}>
                 <div className="col-6" style={{width: "50%", float: "left"}}>
-                    <h5>Made by Liz Clawson</h5>
+                    <h4>Made by Liz Clawson</h4>
                 </div>
                 <div className="col-6" style={{width: "50%", float: "right"}}>
-                    <SocialIcon url="https://www.linkedin.com/in/elizabeth-clawson-175904169/"/>
-                    <SocialIcon url="http://acnhapi.com/"/>
-                    <SocialIcon url="https://github.com/e-clawson"/>
+                    <SocialIcon url="https://www.linkedin.com/in/elizabeth-clawson-175904169/" style={{width:"2em", margin:".2em", padding:".2em"}}/>
+                    <SocialIcon url="http://acnhapi.com/" style={{width:"2em", margin:".2em", padding:".2em"}}/>
+                    <SocialIcon url="https://github.com/e-clawson" style={{width:"2em", margin:".2em", padding:".2em"}}/>
                 </div>
             </div>
         </div>
