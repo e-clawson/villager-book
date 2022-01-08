e38f53e0f80311219676acab708ed4365f44b40c
Elizabeth Clawson <emclawson1@gmail.com>
2022-01-08 13:00:48 -0600

    add imports and routes to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,44 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { VillagerForm } from './Villagers/VillagerForm';
+import { VillagerContainer } from '../containers/VillagerContainer';
+import { Home } from '../Home';
+import { VillagerCard } from './Villagers/VillagerCard';
+import {Navbar} from './navigation/Navbar';
+import {Header} from './navigation/Header';
+import {Home} from './Home';
+import {About} from './About';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Header />
+        <Switch>
+          <Route path="/villagers/new">
+            <VillagerForm />
+          </Route>
+
+          <Route path="/villagers/:id">
+            <VillagerCard />
+          </Route>
+
+          <Route path="/villagers">
+            <VillagerContainer />
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
